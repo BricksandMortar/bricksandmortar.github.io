f7747c9170b397701aac4f166b5af9352600cff5
Arran France <arranf@gmail.com>
2016-04-21 17:06:32 +0100

    HTML-Proofing

--- script/citest.sh
+++ script/citest.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 #Runs htmlproofer, build fails if proofer fails
-bundle exec htmlproofer ./_site --allow-hash-href
+bundle exec htmlproofer ./_site --allow-hash-href --enforce_https --check_favicon --assume_extension
