937d66f851720f49e4f1d26cff3ef300b2b4774f
Arran France <arranf@gmail.com>
2016-04-21 17:11:26 +0100

    Change to plugins and fix html-proofer commands

--- script/citest.sh
+++ script/citest.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 #Runs htmlproofer, build fails if proofer fails
-bundle exec htmlproofer ./_site --allow-hash-href --enforce_https --check_favicon --assume_extension
+bundle exec htmlproofer ./_site --allow-hash-href --enforce-https --check-favicon --assume-extension
