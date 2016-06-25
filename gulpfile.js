97af230907c2b25d148cd74aba385a0688de9b2a
Arran France <arranf@gmail.com>
2016-06-25 22:14:45 +0100

    Bower and Gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,73 @@
 var gulp = require('gulp'),
-gp_less = require('gulp-less'),
-gp_concat = require('gulp-concat'),
-gp_rename = require('gulp-rename'),
-gp_uglify = require('gulp-uglify'),
-pump = require('pump')
-cssnano = require('gulp-cssnano'),
-jshint = require('gulp-jshint'),
-imagemin = require('gulp-imagemin');
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump');
 
 var config = {
     src: "./assets/src/",
     dist: "./assets/dist/"
-}
+};
 
-gulp.task('minify-markdown', function (cb) {
-  pump([
-        gulp.src([config.src+'jquery-1.12.2.min.js', config.src+'/js/bootstrap.min.js', config.src+'/js/docs.min.js']),
-        gp_concat('moment_showdown.min.js'),
-        uglify(),
-        gulp.dest(config.dist+'js')
-    ],
-    cb
-  );
-});
+gulp.task('default', ['minify-markdown', 'minify-main', 'less', 'images', 'fontawesome-css', 'fontawesome-fonts']);
 
-gulp.task('minify-main', function (cb) {
-  pump([
-        gulp.src([config.src+'js/moment.min.js', config.src+'js/showdown.min.js']),
-        gp_concat('default.min.js'),
-        uglify(),
-        gulp.dest(config.dist+'js')
-    ],
-    cb
-  );
-});
-
-//TODO Check
-gulp.task('less', function (cb) {
-    var less_source = config.src+'less/';
+//Compile LESS and minify JS
+gulp.task('minify-markdown', function(cb) {
     pump([
-        gulp.src(less_source+'style.less'),
-        gp_less({
-          paths: [ less_source+'bootstrap-3.3.6/' ]
-        )
-    ])
-    .pipe(gulp.dest('./assets/src/css'));
+            gulp.src(['./bower_components/moment/min/moment.min.js', './bower_components/showdown/dist/showdown.min.js'
+        ]),
+            concat('moment_showdown.min.js'),
+            uglify(),
+            gulp.dest(config.dist + 'js')
+        ],
+        cb
+    );
+});
+
+gulp.task('minify-main', function(cb) {
+    pump([
+            gulp.src(['./bower_components/jquery/dist/jquery.min.js', config.src + '/js/bootstrap.min.js', config.src + '/js/docs.min.js'
+            // ,  './bower_components/jquery.scrollTo/jquery.scrollTo.js'
+        ]),
+            concat('main.min.js'),
+            uglify(),
+            gulp.dest(config.dist + 'js')
+        ],
+        cb
+    );
+});
+
+gulp.task('less', function(cb) {
+    var less_source = config.src + 'less/';
+    pump([
+            gulp.src(less_source + 'style.less'),
+            less({
+                paths: ['./assets/src/less/bootstrap-3.3.6/']
+            }),
+            gulp.dest(config.dist + 'css')
+        ],
+        cb);
+});
+
+gulp.task('images', function(cb) {
+    pump([gulp.src(config.src + '/images/**/*'),
+            gulp.dest(config.dist + '/images')
+        ],
+        cb);
+});
+
+//Copy FontAwesome
+gulp.task('fontawesome-css', function(cb) {
+    pump([gulp.src('./bower_components/font-awesome/css/font-awesome.min.css'),
+            gulp.dest(config.dist + 'css')
+        ],
+        cb);
+});
+
+gulp.task('fontawesome-fonts', function(cb) {
+    pump([gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg,woff2}'),
+            gulp.dest(config.dist + 'fonts')
+
+        ],
+        cb);
 });
